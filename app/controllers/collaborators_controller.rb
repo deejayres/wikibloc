669dd426337e94679abe82dd5d84694caaf35901
deejayres <erinresso@gmail.com>
2016-04-21 20:05:56 -0700

    create collaborator from wiki show, validate collaborator

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,6 +1,18 @@
 class CollaboratorsController < ApplicationController
+
   def create
-    binding.pry
+    wiki = Wiki.find(params[:wiki])
+    email = params[:collaborators_email]
+    user = User.find_by(email: email)
+
+    collaborator = wiki.collaborators.new(user: user)
+
+    if collaborator.save
+      flash[:notice] = "Collaborator Added."
+    else
+      flash[:alert] = "Could not find a user with that email address. Please try again"
+    end
+    redirect_to wiki
   end
 
   def destroy
