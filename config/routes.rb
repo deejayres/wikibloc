9378206928e1c507d1828f5d7ea5f94805565bff
deejayres <erinresso@gmail.com>
2016-04-25 15:47:26 -0700

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-  # resources :users, only: :show
-
   get "profile" => "users#show", :as => 'profile'
 
   resources :wikis
-    # resources :collaborators, only: [:create, :destroy]
-
 
   devise_for :users
 
